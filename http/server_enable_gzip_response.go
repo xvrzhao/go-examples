0a465648a580c5269d8335a693a08d94ac95118f
Xavier Zhao <xvrzhao@gmail.com>
2019-12-13 11:18:15 +0800

    bug fix: close the gzipWriter to flush unwritten data to underlayer

--- http/server_enable_gzip_response.go
+++ http/server_enable_gzip_response.go
@@ -6,28 +6,26 @@ import (
 	"net/http"
 )
 
+// response with gzip encoding
 func RunServerEnableGzip(addr string) {
 	serveMux.HandleFunc("/test/gzip", func(writer http.ResponseWriter, request *http.Request) {
+		msg := []byte("Hello, Gzip!")
+		// 0. the following operation assume that the client supports the gzip encoding, so it is necessary
+		//	  to determine whether to enable gzip response by retrieving request's `accept-encoding` header.
+
 		// 1. set header: content encoding and type, can't set one without the other
 		writer.Header().Set("content-encoding", "gzip")
-		writer.Header().Set("content-type", "text/plain; charset=utf-8")
+		writer.Header().Set("content-type", http.DetectContentType(msg))
 
 		// 2. wrap the writer
 		gzipWriter := gzip.NewWriter(writer)
+		defer gzipWriter.Close() // remember to close because this will also flush any unwritten data to underlying writer
 
 		// 3. write data by wrapper
-		_, err := gzipWriter.Write([]byte("hello gzip!"))
+		_, err := gzipWriter.Write(msg)
 		if err != nil {
-			http.Error(writer, "server error", 500)
+			http.Error(writer, "server error", http.StatusInternalServerError)
 			log.Printf("failed to write compressed stream: %v", err)
-			return
-		}
-
-		// 4. remember to flush!!
-		err = gzipWriter.Flush()
-		if err != nil {
-			http.Error(writer, "server error", 500)
-			log.Printf("failed to flush compressed stream: %v", err)
 			return
 		}
 	})
