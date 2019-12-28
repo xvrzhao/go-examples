98937c47a0b78ca248a04e9ddb5401a65356ef37
Xavier Zhao <xvrzhao@gmail.com>
2019-12-28 17:25:13 +0800

    mod: reflect.RunSetStructExample

--- reflect/general.go
+++ reflect/general.go
@@ -25,15 +25,28 @@ type person struct {
 	gender uint8
 }
 
+func (p person) tellGender() {
+	fmt.Println(p.gender)
+}
+
+func (p person) TellName() {
+	fmt.Println(p.Name)
+}
+
+func (p *person) Grow(years uint8) {
+	p.Age += years
+}
+
 func RunSetStructExample() {
 	v := reflect.ValueOf(person{})
-	fmt.Println(v.CanSet()) // false
+	fmt.Println(v.CanSet(), v.NumField(), v.NumMethod()) // false 3 1(methods that their receiver is `person` and are exported)
 
 	v = reflect.ValueOf(&person{})
-	fmt.Println(v.CanSet()) // false
+	fmt.Println(v.CanSet(), v.NumMethod()) // false 2(methods that their receiver could be `person` or `*person` and are exported)
+	// fmt.Println(v.NumField()) // panic: reflect: call of reflect.Value.NumField on ptr Value
 
 	v = reflect.ValueOf(&person{}).Elem()
-	fmt.Println(v.CanSet()) // true
+	fmt.Println(v.CanSet(), v.NumField(), v.NumMethod()) // true 3 1(methods that their receiver is `person` and are exported)
 
 	p := person{}
 	v = reflect.ValueOf(&p).Elem()
@@ -43,9 +56,14 @@ func RunSetStructExample() {
 	fmt.Println(p.Name) // Xavier
 
 	gv := v.FieldByName("gender")
-	fmt.Println(gv.Kind(), gv.CanSet()) // uint8 false(gender is unexported)
+	fmt.Println(gv.Kind(), gv.Uint(), gv.CanSet()) // uint8 0(value is accessible even though gender is unexported) false(gender is unexported)
 
 	zv := v.FieldByName("notExistField") // sv is zero Value
 	// fmt.Println(sv.IsZero()) // panic: reflect: call of reflect.Value.IsZero on zero Value
 	fmt.Println(zv.IsValid(), zv.CanSet()) // false false
+
+	tellName := v.MethodByName("TellName")
+	if tellName.IsValid() {
+		tellName.Call(nil) // Xavier
+	}
 }
