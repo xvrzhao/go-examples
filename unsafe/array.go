3747f3a1b1228fec3b923c3cd5431e013b58d82c
Xavier Zhao <xvrzhao@gmail.com>
2020-01-26 00:48:18 +0800

    add: unsafe.ArrayOffsetElement

--- unsafe/array.go
+++ unsafe/array.go
@@ -6,6 +6,15 @@ import (
 	"unsafe"
 )
 
+// ArrayOffsetElement 演示了获取数组元素地址的两种方式。
+func ArrayOffsetElement() {
+	arr := [5]int{}
+	offset := 3
+
+	fmt.Printf("%p \n", &arr[offset])                                                          // 0xc000014198
+	fmt.Printf("0x%x \n", uintptr(unsafe.Pointer(&arr))+uintptr(offset)*unsafe.Sizeof(arr[0])) // 0xc000014198
+}
+
 // ArraySliceDiff 演示了 slice 与 array 本质上的区别。
 // slice 是一个 reflect.SliceHeader 结构，而 array 是一段连续的内存结构。
 func ArraySliceDiff() {
--- unsafe/array_test.go
+++ unsafe/array_test.go
@@ -2,6 +2,10 @@ package unsafe
 
 import "testing"
 
+func TestArrayOffsetElement(t *testing.T) {
+	ArrayOffsetElement()
+}
+
 func TestArraySliceDiff(t *testing.T) {
 	ArraySliceDiff()
 }
