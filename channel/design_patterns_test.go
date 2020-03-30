913db1bd139a9caa169cb432eef5d7b14cb28311
Xavier Zhao <xvrzhao@gmail.com>
2020-03-30 15:04:02 +0800

    add: FindPrimeNumbersBySieves and FindPrimeNumbersBySievesFac

--- channel/design_patterns_test.go
+++ channel/design_patterns_test.go
@@ -5,3 +5,11 @@ import "testing"
 func TestFactoryPattern(t *testing.T) {
 	FactoryPattern()
 }
+
+func TestFindPrimeNumbersBySieves(t *testing.T) {
+	FindPrimeNumbersBySieves()
+}
+
+func TestFindPrimeNumbersBySievesFac(t *testing.T) {
+	FindPrimeNumbersBySievesFac()
+}
