5181e2d089d09bb7379626eb48e1acd3432d585d
Xavier Zhao <xvrzhao@gmail.com>
2020-03-31 15:25:20 +0800

    add: ChildGoroutinePanic, PanicTerminateWholeProgram and RecoverMustInDefer, resolve #3

--- panic_defer_recover/panic_recover_test.go
+++ panic_defer_recover/panic_recover_test.go
@@ -5,3 +5,15 @@ import "testing"
 func TestReturnedInterfaceValue(t *testing.T) {
 	ReturnedInterfaceValue()
 }
+
+func TestChildGoroutinePanic(t *testing.T) {
+	ChildGoroutinePanic()
+}
+
+func TestPanicTerminateWholeProgram(t *testing.T) {
+	PanicTerminateWholeProgram()
+}
+
+func TestRecoverMustInDefer(t *testing.T) {
+	RecoverMustInDefer()
+}
